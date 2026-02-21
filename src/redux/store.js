a0bd6d4717513132664a1878cfbf6ac1318fa118
heatherd2025 <heatherdeliso@gmail.com>
2026-02-21 17:47:07 -0500

    fixed store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,42 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import authReducer from "../features/auth/authSlice.js";
+// import { scienceNewsApi } from "../features/feeds/scienceNewsApi.js";
+// import { userApi } from "../features/user/userApi.js";
+// import { spaceNewsApi } from "../features/feeds/spaceNewsApi.js";
+// import { spaceImagesApi } from "../features/feeds/apodApi.js";
+// import { currentsApi } from "../features/feeds/currentsApi.js";
+
+// const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//     [scienceNewsApi.reducerPath]: scienceNewsApi.reducer,
+//     [spaceNewsApi.reducerPath]: spaceNewsApi.reducer,
+//     [spaceImagesApi.reducerPath]: spaceImagesApi.reducer,
+//     [userApi.reducerPath]: userApi.reducer,
+//     [currentsApi.reducerPath]: currentsApi.reducer,
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware()
+//       .concat(scienceNewsApi.middleware)
+//       .concat(spaceNewsApi.middleware)
+//       .concat(spaceImagesApi.middleware)
+//       .concat(userApi.middleware)
+//       .concat(currentsApi.middleware),
+// });
+
+// export default store;
+
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice.js";
-import { scienceNewsApi } from "../features/feeds/scienceNewsApi.js";
-import { userApi } from "../features/user/userApi.js";
-import { spaceNewsApi } from "../features/feeds/spaceNewsApi.js";
-import { spaceImagesApi } from "../features/feeds/apodApi.js";
-import { currentsApi } from "../features/feeds/currentsApi.js";
+import { api } from "../api/baseApi.js";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    [scienceNewsApi.reducerPath]: scienceNewsApi.reducer,
-    [spaceNewsApi.reducerPath]: spaceNewsApi.reducer,
-    [spaceImagesApi.reducerPath]: spaceImagesApi.reducer,
-    [userApi.reducerPath]: userApi.reducer,
-    [currentsApi.reducerPath]: currentsApi.reducer,
+    [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-      .concat(scienceNewsApi.middleware)
-      .concat(spaceNewsApi.middleware)
-      .concat(spaceImagesApi.middleware)
-      .concat(userApi.middleware)
-      .concat(currentsApi.middleware),
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export default store;
